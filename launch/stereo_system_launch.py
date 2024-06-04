d8722c0466110a7db39160745cb244743c5199f9
Nedim Hosic <nedim.hosic1@gmail.com>
2024-06-05 00:40:48 +0200

    Add launch file for starting stereo camera nodes

---
+++ launch/stereo_system_launch.py
@@ -0,0 +1,24 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+def generate_launch_description():
+    return LaunchDescription([
+        Node(
+            package='autonomous_navigation',
+            executable='frame_publisher',
+            name='left_frame_publisher',
+            ros_arguments=['-p', 'camera_id:=0'],
+            remappings=[
+                ('/left_frame_publisher/camera_0/image', '/left_camera/image')
+            ]
+        ),
+        Node(
+            package='autonomous_navigation',
+            executable='frame_publisher',
+            name='right_frame_publisher',
+            ros_arguments=['-p', 'camera_id:=1'],
+            remappings=[
+                ('/right_frame_publisher/camera_1/image', '/right_camera/image')
+            ]
+        )
+    ])
