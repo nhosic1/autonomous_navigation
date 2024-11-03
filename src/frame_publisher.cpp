d9c06c953773b968732d3424f70e980bc52c7d47
Nedim Hosic <nedim.hosic1@gmail.com>
2024-11-04 00:08:24 +0100

    Rotate published frames by 180 deg

--- src/frame_publisher.cpp
+++ src/frame_publisher.cpp
@@ -44,6 +44,9 @@ public:
         stream_config.size.height = frame_height;
         stream_config.pixelFormat = formats::RGB888;
 
+        // Rotate image
+        config->orientation = Orientation::Rotate180;
+
         // Validate the config (original config might be modified, if invalid)
         config->validate();
         camera_->configure(config.get());
