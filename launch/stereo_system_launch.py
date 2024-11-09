ee7fbb0e43a2f7b8b005947a1903f3ce17c2d744
Nedim Hosic <nedim.hosic1@gmail.com>
2024-11-09 17:44:01 +0100

    Set JPEG compression quality to 50%

--- launch/stereo_system_launch.py
+++ launch/stereo_system_launch.py
@@ -9,20 +9,34 @@ def generate_launch_description():
                 package="autonomous_navigation",
                 executable="frame_publisher",
                 name="left_frame_publisher",
-                ros_arguments=["-p", "camera_id:=0"],
+                parameters=[
+                    {"camera_id": 0},
+                    {"left_frame_publisher.camera_0.image.compressed.jpeg_quality": 50},
+                ],
                 remappings=[
                     ("/left_frame_publisher/camera_0/image", "/left_camera/image"),
-                    ("/left_frame_publisher/camera_0/image/compressed", "/left_camera/image/compressed")
+                    (
+                        "/left_frame_publisher/camera_0/image/compressed",
+                        "/left_camera/image/compressed",
+                    ),
                 ],
             ),
             Node(
                 package="autonomous_navigation",
                 executable="frame_publisher",
                 name="right_frame_publisher",
-                ros_arguments=["-p", "camera_id:=1"],
+                parameters=[
+                    {"camera_id": 1},
+                    {
+                        "right_frame_publisher.camera_1.image.compressed.jpeg_quality": 50
+                    },
+                ],
                 remappings=[
                     ("/right_frame_publisher/camera_1/image", "/right_camera/image"),
-                    ("/right_frame_publisher/camera_1/image/compressed", "/right_camera/image/compressed")
+                    (
+                        "/right_frame_publisher/camera_1/image/compressed",
+                        "/right_camera/image/compressed",
+                    ),
                 ],
             ),
         ]
