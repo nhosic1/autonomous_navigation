4f1a361c1c1de8aac53c32bfba6ec3e7d6660e0c
Nedim Hosic <nedim.hosic1@gmail.com>
2024-11-18 13:31:11 +0100

    Fix typo

--- src/localization.cpp
+++ src/localization.cpp
@@ -85,7 +85,7 @@ namespace loc
                 if (point_2D == point_2D_matched_prev)
                 {
                     // Ignore matched points inconsistent with estimated position
-                    const cv::Point2d point_2D_proj(static_cast<double>(points_2D_proj[i].x), static_cast<double>(points_2D_proj[i].y));;
+                    const cv::Point2d point_2D_proj(static_cast<double>(points_2D_proj[i].x), static_cast<double>(points_2D_proj[i].y));
                     if (cv::norm(point_2D_matched - point_2D_proj) < 40.0)
                     {
                         points_2D_pnp.push_back(point_2D_matched);
