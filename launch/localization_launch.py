1d4c48d9a1596268fa29d2d4672c34c5e6a4c7d4
Nedim Hosic <nedim.hosic1@gmail.com>
2026-04-07 12:02:08 +0200

    Launch alignemnt node only in simulation

--- launch/localization_launch.py
+++ launch/localization_launch.py
@@ -1,6 +1,7 @@
 import os
 from launch import LaunchDescription
 from launch.actions import DeclareLaunchArgument, TimerAction
+from launch.conditions import IfCondition
 from launch.substitutions import LaunchConfiguration
 from launch_ros.actions import Node
 from ament_index_python.packages import get_package_share_directory
@@ -77,6 +78,7 @@ def generate_launch_description():
         executable="gazebo_odom_aligner",
         output="screen",
         parameters=[{"use_sim_time": sim}],
+        condition=IfCondition(sim),
     )
 
     return LaunchDescription(
